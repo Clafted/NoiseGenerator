2a071baced69454a7308e9ea190a2dd3fd64072b
Noah Miguel Perez <76635402+Clafted@users.noreply.github.com>
2025-07-16 20:50:43 -0400

    Minor formatting changes

--- src/NoiseGenerator.cpp
+++ src/NoiseGenerator.cpp
@@ -13,34 +13,44 @@ using namespace std::chrono;
 
 float distance(vec2 a, vec2 b);
 
-std::vector<float> NoiseGenerator::generatePerlinNoise(int finalWidth, int finalHeight, int spacing, int intervals) {
+std::vector<float> NoiseGenerator::generatePerlinNoise(int finalWidth, int finalHeight, int spacing, int intervals) 
+{
     if (intervals <= 0) return std::vector<float>(finalWidth*finalHeight);
+    
     // Generate gradients
     double theta;
     std::vector<vec2> normals((finalWidth / spacing + 1) * (finalHeight / spacing + 1));
     high_resolution_clock::time_point start = high_resolution_clock::now();
-    for (int i = 0; i < normals.size(); i++) {
+    
+    for (int i = 0; i < normals.size(); i++) 
+    {
         srand(1000000000 * duration_cast<duration<double>>(high_resolution_clock::now() - start).count());
         theta = (rand() % 360)*3.1415 / 180.0;
         normals[i] = vec2(sin(theta), cos(theta));
     }
     std::shuffle(normals.begin(), normals.end(), std::default_random_engine(rand()));
+    
     // Calculate pixel colors
     vec2 g0, g1, g2, g3;
     float dot0, dot1, dot2, dot3;
     float xI1, xI2, finalColor;
     std::vector<float> result(finalWidth*finalHeight);
+    
     // Traverse cells
     for (int gX = 0; gX < finalWidth/spacing; gX++) {
-        for (int gY = 0; gY < finalHeight/spacing; gY++) {
+        for (int gY = 0; gY < finalHeight/spacing; gY++) 
+        {
             // Find the four corner normals
             g0 = normals[(finalWidth/spacing + 1)*gY + gX];
             g1 = normals[(finalWidth/spacing + 1)*gY + gX+1];
             g2 = normals[(finalWidth/spacing + 1)*(gY+1) + gX];
             g3 = normals[(finalWidth/spacing + 1)*(gY+1) + gX+1];
+            
             // Traverse pigxels
-            for (int x = 0; x < spacing; x++) {
-                for (int y = 0; y < spacing; y++) {
+            for (int x = 0; x < spacing; x++) 
+            {
+                for (int y = 0; y < spacing; y++) 
+                {
                     // Calculate dot-products with four corners
                     dot0 = dotProduct(vec2(x, y) / (spacing-1.0), g0);
                     dot1 = dotProduct(vec2(x-(spacing-1), y) / ((spacing-1)-1.0), g1);
@@ -71,7 +81,8 @@ std::vector<float> NoiseGenerator::generateWorleyNoise(int width, int height, in
 
     high_resolution_clock::time_point start = high_resolution_clock::now();
     for (int cX = 0; cX < width/spacing + 1; cX++) {
-        for (int cY = 0; cY < width/spacing + 1; cY++) {
+        for (int cY = 0; cY < width/spacing + 1; cY++) 
+        {
             srand(1000000000 * duration_cast<duration<double>>(high_resolution_clock::now() - start).count());
             points[cY*(width/spacing + 1) + cX].x = cX*spacing + rand() % spacing;
             srand(1000000000 * duration_cast<duration<double>>(high_resolution_clock::now() - start).count());
@@ -82,7 +93,8 @@ std::vector<float> NoiseGenerator::generateWorleyNoise(int width, int height, in
     vec2 pixel, closest;
     // Traverse cells
     for (int cX = 0; cX < width / spacing; cX++) {
-        for (int cY = 0; cY < height / spacing; cY++) {
+        for (int cY = 0; cY < height / spacing; cY++) 
+        {
             c0 = points[cY * (width / spacing + 1) + cX];
             c1 = points[cY * (width / spacing + 1) + cX + 1];
             c2 = points[(cY+1) * (width / spacing + 1) + cX];
@@ -90,7 +102,8 @@ std::vector<float> NoiseGenerator::generateWorleyNoise(int width, int height, in
     
             // Traverse pixels
             for(int x = 0; x < spacing; x++) {
-                for(int y = 0; y < spacing; y++) {
+                for(int y = 0; y < spacing; y++) 
+                {
                     pixel = vec2(x + cX*spacing, y + cY*spacing);
                     closest = c0;
                     if (distance(pixel, c1) < distance(pixel, closest)) closest = c1;
@@ -107,5 +120,5 @@ std::vector<float> NoiseGenerator::generateWorleyNoise(int width, int height, in
 }
 
 float distance(vec2 a, vec2 b) {
-    return sqrt(pow(a.x - b.x, 2) + pow(a.y - b.y, 2));
+    return sqrt((a.x-b.x)(a.x-b.x) + (a.y-b.y)(a.y-b.y));
 }
